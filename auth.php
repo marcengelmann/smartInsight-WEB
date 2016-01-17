6eb83863308b609533638539e7427b5422ea850b
Marc Engelmann <marc.engelmann.92@gmail.com>
2016-01-17 14:39:14 +0100

    minor updates

--- auth.php
+++ auth.php
@@ -1,10 +1,6 @@
 <?php
 ini_set('session.use_cookies', '1');
 session_start();
-//print_r($_SESSION);
-//echo($_SESSION['username']);
-//echo($_SESSION["username"]);
-//echo("HÄÄÄ!=!=!=!");
 if(!isset($_SESSION["username"])){
 header("Location: login.php");
 echo("should have done something!");
--- index.php
+++ index.php
@@ -4,20 +4,3 @@ header("Location: dashboard.php");
 echo("should have done something!");
 exit(); }
 ?>
-    <!--
-    <!DOCTYPE html>
-    <html>
-
-    ?php include("header.html"); ?>
-
-        <body>
-            <div class="form">
-                <p>Welcome
-                    ?php echo $_SESSION['username']; ?>!</p>
-                <p>This is secure area.</p>
-                <p><a href="dashboard.php">Dashboard</a></p>
-                <a href="logout.php">Logout</a>
-            </div>
-        </body>
-
-    </html>-->
